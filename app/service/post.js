88d4a81e52374e83b4eb496e2906dfd0a976e1f4
coolucifer <1045606768@qq.com>
2018-12-04 17:12:22 +0800

    修正语法

--- app/service/post.js
+++ app/service/post.js
@@ -6,7 +6,7 @@ class PostService extends Service{
     const { ctx } = this;
     // inclusion模式 指定返回的键，不返回其他键
     // .sort(): 按id排序
-    return ctx.model.Post.find({}, { title: 1, author: 1, createTime: 1, updateTime: 1 }).sort({ '_id': -1 });
+    return await ctx.model.Post.find({}, { title: 1, author: 1, createTime: 1, updateTime: 1 }).sort({ '_id': -1 });
   }
   async create(params) {
     const { ctx } = this;
