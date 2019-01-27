e0c2a22098b99ac0bd576bff6b2386324937e32f
victor <1045606768@qq.com>
2019-01-27 11:19:12 +0800

    添加socket.io

--- config/config.default.js
+++ config/config.default.js
@@ -20,13 +20,28 @@ exports.mongoose = {
   client: {
     // url: 'mongodb://coolucifer:Inuyasha0425@89.208.248.45:27017/admin',
     url: 'mongodb://manager:main_db_manager@89.208.248.45:27017/main',
-    options: {}
-  }
+    options: {},
+  },
 };
 
 // 安全配置
 exports.security = {
   csrf: {
-    enable: false
+    enable: false,
+  },
+};
+
+// socket.io
+exports.io = {
+  init: {},
+  namespace: {
+    '/': {
+      connectionMiddleware: [],
+      packetMiddleware: [],
+    },
+    '/example': {
+      connectionMiddleware: [],
+      packetMiddleware: [],
+    },
   },
 };
