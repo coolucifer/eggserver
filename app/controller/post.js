122d9fb538731712000ee13cb2f688b476d322dd
victor <1045606768@qq.com>
2019-03-23 21:55:27 +0800

    格式化文件

--- app/controller/post.js
+++ app/controller/post.js
@@ -1,13 +1,13 @@
-const Controller = require('egg').Controller;
+const { Controller } = require('egg');
 
 class PostController extends Controller {
   async list() {
     const { ctx, app } = this;
-    let msg = await ctx.service.post.list()
+    const msg = await ctx.service.post.list()
       .then(res => {
-        const list = res.map(item => {
+        const list = res.map(i => {
           // 解决无法编辑返回数据的问题
-          item = item.toObject();
+          const item = i.toObject();
           item.id = item._id;
           delete item._id;
           item.createTime = app.toLocalTime(item.createTime);
@@ -16,39 +16,32 @@ class PostController extends Controller {
         });
         return { success: true, resultCode: '100', list };
       })
-      .catch(err => {
-        return { success: false, err: err }
-      });
+      .catch(err => ({ success: false, err }));
     ctx.body = msg;
   }
+
   async create() {
     const { ctx } = this;
     const { body: params } = ctx.request;
-    let msg = await ctx.service.post.create(params)
-      .then(res => {
-        return { success: true, resultCode: '100', id: res._id };
-      })
-      .catch(err => {
-        return { success: false, err: err };
-      });
+    const msg = await ctx.service.post.create(params)
+      .then(res => ({ success: true, resultCode: '100', id: res._id }))
+      .catch(err => ({ success: false, err }));
     ctx.body = msg;
   }
+
   async update() {
     const { ctx } = this;
     const { body: params } = ctx.request;
     const msg = await ctx.service.post.update(params._id, params)
-      .then(res => {
-        return { success: true, resultCode: '100', id: res._id };
-      })
-      .catch(err => {
-        return { success: false, err: err };
-      });
+      .then(res => ({ success: true, resultCode: '100', id: res._id }))
+      .catch(err => ({ success: false, err }));
     ctx.body = msg;
   }
+
   async query() {
     const { ctx, app } = this;
     const { id: params } = ctx.request.query;
-    let msg = await ctx.service.post.query(params)
+    const msg = await ctx.service.post.query(params)
       .then(res => {
         // 根据Id获取的文章是唯一的, 取查询到的数组的第0位
         let post = res[0];
@@ -59,11 +52,9 @@ class PostController extends Controller {
         post.updateTime = app.toLocalTime(post.updateTime);
         return { success: true, resultCode: '100', post };
       })
-      .catch(err => {
-        return { success: false, err: err };
-      });
+      .catch(err => ({ success: false, err }));
     ctx.body = msg;
   }
-};
+}
 
 module.exports = PostController;
--- app/service/post.js
+++ app/service/post.js
@@ -1,26 +1,35 @@
-const Service = require('egg').Service;
+const { Service } = require('egg');
 const mongoose = require('mongoose');
 
-class PostService extends Service{
+class PostService extends Service {
   async list() {
     const { ctx } = this;
     // inclusion模式 指定返回的键，不返回其他键
     // .sort(): 按id排序
-    return await ctx.model.Post.find({}, { title: 1, author: 1, createTime: 1, updateTime: 1 }).sort({ '_id': -1 });
+    const res = await ctx.model.Post.find({}, {
+      title: 1, author: 1, createTime: 1, updateTime: 1,
+    }).sort({ _id: -1 });
+    return res;
   }
+
   async create(params) {
     const { ctx } = this;
-    return await ctx.model.Post.create(params);
+    const res = await ctx.model.Post.create(params);
+    return res;
   }
+
   async update(id, params) {
     const { ctx } = this;
-    return await ctx.model.Post.update({ '_id': id }, params);
+    const res = await ctx.model.Post.update({ _id: id }, params);
+    return res;
   }
+
   async query(postId) {
     const { ctx } = this;
-    const _id = mongoose.Types.ObjectId(postId);
-    return await ctx.model.Post.find({ _id });
+    const id = mongoose.Types.ObjectId(postId);
+    const res = await ctx.model.Post.find({ _id: id });
+    return res;
   }
-};
+}
 
 module.exports = PostService;
