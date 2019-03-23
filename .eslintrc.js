32033e132012ecb34db864049810a284e2d867a4
victor <1045606768@qq.com>
2019-03-23 21:53:55 +0800

    更新eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   extends: "airbnb-base",
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-plusplus': [
+      process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      { allowForLoopAfterthoughts: true },
+    ],
     'arrow-parens': [
       process.env.NODE_ENV === 'production' ? 'error' : 'off',
       'as-needed',
     ],
-  }
+    'max-len': ['warn', 200],
+    'linebreak-style': ["off", "windows"],
+    'no-underscore-dangle': ["off"],
+  },
 };
