6f5156d4bf7d428fd85fe697d60fcf57794f3690
victor <1045606768@qq.com>
2019-07-03 21:25:30 +0800

    修改页面标题

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: 'Doco.dev',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
