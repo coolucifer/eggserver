db4d0c37e2cb90c854200f593daa2298ea14a6b6
coolucifer <1045606768@qq.com>
2019-07-08 16:12:12 +0800

    更新socket

--- app/io/middleware/connection.js
+++ app/io/middleware/connection.js
@@ -1,6 +1,7 @@
 // 在每一个客户端连接或者退出时发生作用，故而我们通常在这一步进行授权认证，对认证失败的客户端做出相应的处理
 module.exports = () => async (ctx, next) => {
-  console.log('a user connected');
+  const { socket } = ctx;
+  console.log('a user connected: ', socket.id);
   ctx.socket.emit('res', 'connected!');
   await next();
   // execute when disconnect
